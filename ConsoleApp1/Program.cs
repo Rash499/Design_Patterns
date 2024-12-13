8f3ca2eb8cabd056d2e86da7388590fba36cb3e0
Rash499 <rashmikadilmin499@gmail.com>
2024-12-13 14:23:10 +0530

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿//Encapsulation
+
+using ConsoleApp1.src.OOP.Encapsulation;
+
+BadBankAccount badAccount = new BadBankAccount();
+badAccount.balance = 100;
+
+System.Console.WriteLine(badAccount.balance);
+
